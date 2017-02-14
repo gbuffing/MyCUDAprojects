124a0efff934c33da254388b8cff27ccc7b7be08
gbuffing <gbuffing@gmail.com>
2017-02-14 16:00:29 -0600

    double precision random generation

--- monte/template.cu
+++ monte/template.cu
@@ -18,16 +18,23 @@
 // declaration, forward
 void pi(int argc, char **argv);
 
-/********************** DEVICE CODE *********************/
+
 // some hints here
 // http://stackoverflow.com/questions/11832202/cuda-random-number-generating
 //
 // this is pretty good...look at multi core implementation at bottom
 // http://cs.umw.edu/~finlayson/class/fall16/cpsc425/notes/cuda-random.html
+//
+// good article
+// http://stackoverflow.com/questions/26650391/generate-random-number-within-a-function-with-curand-without-preallocation
 
-__global__ void random(float *result)  {
 
-    curandState_t state;
+
+/********************** DEVICE CODE *********************/
+
+__global__ void random(double *result)  {
+
+    curandState_t state;  // need a local state for each thread, see cs.umw.edu
 
     curand_init(1234ULL, threadIdx.x, 0, &state);
 // arg1 -> the seed controls the sequence of random values that are produced
@@ -35,7 +42,7 @@ __global__ void random(float *result)  {
 // arg3 -> offset, how much extra we advance in the sequence for each call, can be 0 
 // curand works like rand - except that it takes a state as a parameter 
     
-    float tmp = curand_uniform(&state);
+    double tmp = curand_uniform_double(&state);
     result[threadIdx.x] = tmp;
 }
 
@@ -58,18 +65,23 @@ void pi(int argc, char **argv)
 
 
     int n = 1024;
-    float size = n * sizeof(float);
-    float *gpu_x;
+    double size = n * sizeof(double);
+    double *gpu_x;
     cudaMalloc((void **) &gpu_x, size);
 
     random<<<1,n>>>(gpu_x);
 
-    float *x;
-    x = (float *)malloc(size);
+    double *x;
+    x = (double *)malloc(size);
     cudaMemcpy(x, gpu_x, size, cudaMemcpyDeviceToHost);
 
-    for (int i=0; i<n; i++)
+    double total = 0.;
+    for (int i=0; i<n; i++) {
         std::cout << x[i] << "\n";
+        total += x[i];
+    }
+
+    std::cout << "\n" << total / double(n) << "\n";
 
     cudaFree(gpu_x);
 
