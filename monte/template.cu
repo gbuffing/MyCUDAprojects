64951614d484b6577509622eceb5a5ea7d34207f
gbuffing <gbuffing@gmail.com>
2017-02-14 15:38:36 -0600

    First version with float random number generator (single precision)

--- monte/template.cu
+++ monte/template.cu
@@ -15,72 +15,36 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
-////////////////////////////////////////////////////////////////////////////////
 // declaration, forward
 void pi(int argc, char **argv);
 
+/********************** DEVICE CODE *********************/
+// some hints here
+// http://stackoverflow.com/questions/11832202/cuda-random-number-generating
 
-#define RANDOM_MAX 100
- 
-__global__ void random(int *result)  {
+__global__ void random(float *result)  {
+
     curandState_t state;
 
-    curand_init(547, 
-                745+threadIdx.x, /*sequence number is only important with multiple cores */
-                1, /*offset is how much extra we advance in the sequence for each call, can be 0 */
-                &state);
-
-    /* curand works like rand - except that it takes a state as a parameter */
-
-    int tmp = curand(&state) % RANDOM_MAX;
-/*    result[0] = tmp;
-
-    tmp = curand(&state) % RANDOM_MAX;
-    result[1] = tmp;  */
-
+    curand_init(1234ULL, threadIdx.x, 0, &state);
+// arg1 -> the seed controls the sequence of random values that are produced
+// arg2 -> sequence number is only important with multiple cores
+// arg3 -> offset, how much extra we advance in the sequence for each call, can be 0 
+// curand works like rand - except that it takes a state as a parameter 
+    
+    float tmp = curand_uniform(&state);
     result[threadIdx.x] = tmp;
 }
 
-__global__ void roll(int n, int *hits, int *throws)  {
-//    int *tmp;
-//    random(tmp);
-
-}
-
 /*
-__global__ void add(int *a, int *b, int *c)  {
-    *c = *a + *b;
-}
-
-__global__ void blockAdd(int *a, int *b, int *c)  {
-    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
-}
-
-__global__ void threadAdd(int *a, int *b, int *c)  {
-    c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
-}
-
 __global__ void threadBlockAdd(int *a, int *b, int *c, int n)  {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
     if (index < n)
         c[index] = a[index] + b[index];
 }
 */
-////////////////////////////////////////////////////////////////////////////////
-// Program main
-////////////////////////////////////////////////////////////////////////////////
-int
-main(int argc, char **argv)
-{
-    pi(argc, argv);
-}
 
-////////////////////////////////////////////////////////////////////////////////
-//! Run a simple test for CUDA
-////////////////////////////////////////////////////////////////////////////////
-
-#define N 2048*2048
-#define THREADS_PER_BLOCK 512 
+/********************** HOST CODE *********************/
 
 void pi(int argc, char **argv)
 {
@@ -91,24 +55,26 @@ void pi(int argc, char **argv)
 
 
     int n = 1024;
-    int size = n * sizeof(int);
-    int *gpu_x;
+    float size = n * sizeof(float);
+    float *gpu_x;
     cudaMalloc((void **) &gpu_x, size);
 
     random<<<1,n>>>(gpu_x);
 
-//    int x;
-    int *x;
-    x = (int *)malloc(size);
-//    cudaMemcpy(&x, gpu_x, size, cudaMemcpyDeviceToHost);
+    float *x;
+    x = (float *)malloc(size);
     cudaMemcpy(x, gpu_x, size, cudaMemcpyDeviceToHost);
 
-//    std::cout << n*sizeof(int) << "  " << sizeof(*x) << "\n";
-
-//    printf("%d\n", x);
     for (int i=0; i<n; i++)
-        printf("%d\n", x[i]);
+        std::cout << x[i] << "\n";
 
     cudaFree(gpu_x);
 
 }
+
+
+// Program main
+int main(int argc, char **argv) {
+    pi(argc, argv);
+}
+
